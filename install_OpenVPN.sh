0fc8002864ddfe5d26ad5f205a9eedacbcb363b3
Jay <mrabtija@gmail.com>
2022-02-18 01:35:54 +0000

    .

--- install_OpenVPN.sh
+++ install_OpenVPN.sh
@@ -887,6 +887,7 @@ client-config-dir /etc/openvpn/ccd
 status /var/log/openvpn/status.log
 verb 3" >>/etc/openvpn/server.conf
 
+
 	# Create client-config-dir dir
 	mkdir -p /etc/openvpn/ccd
 	# Create log dir
